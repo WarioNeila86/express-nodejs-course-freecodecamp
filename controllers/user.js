5993a25469de21c3e04a2b364cc1adf09148a60a
WarioNeila86 <mariogar1001@gmail.com>
2020-08-01 21:16:44 +0200

    Implement sign up controller function

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,9 @@
+const passport = require('passport');
+const models = require('../models');
+const bcrypt = require('bcrypt');
+const myPassport = require('../passport-setup')(passport);
+const flash = require('connect-flash');
+
 exports.showLogin = function(req, res, next) {
     res.render('user/login', {formData: {}, errors: {}});
 }
@@ -10,6 +16,18 @@ exports.login = function(req, res, next) {
     
 }
 
+const generateHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+
 exports.signup = function(req, res, next) {
-    
+    const newUser = models.User.build({
+        email: req.body.email,
+        password: generateHash(req.body.password)
+    });
+    return newUser.save().then(result => {
+        passport.authenticate('local', {
+            successRedirect: '/',
+            failureRedirect: '/signup',
+            failureFlash: true
+        })(req, res, next);
+    });
 }
